5b98ecdab10d96e7c94b1d92673a816b022db37f
kloda <mickeh.se@hotmail.com>
2019-01-06 18:29:50 +0100

    Specify number of bytes (2 or 4) to write for each element in wav file, instead of specifying data type (short or int).

--- Wav.h
+++ Wav.h
@@ -10,10 +10,9 @@ private:
 	const int sampleRate = 44100;
 	const int channelCount = 2;
 	std::vector<SampleType> samples;
-	template<class T>
-	void writeNumber(T number)
+	void writeNumber(int bytes, int number)
 	{
-		outFile.write(reinterpret_cast<const char*>(&number), sizeof(number));
+		outFile.write(reinterpret_cast<const char*>(&number), bytes);
 	}
 public:
 	Wav()
@@ -34,17 +33,17 @@ public:
 		int dataSize = samples.size() * sizeof(SampleType);
 		outFile.open(path, std::ios::binary | std::ios::out);
 		outFile << "RIFF";
-		writeNumber<int>(36 + dataSize);
+		writeNumber(4, 36 + dataSize);
 		outFile << "WAVEfmt ";
-		writeNumber<int>(16);
-		writeNumber<short>(1);
-		writeNumber<short>(2);
-		writeNumber<int>(sampleRate);
-		writeNumber<int>(sampleRate * blockAlign);
-		writeNumber<short>(blockAlign);
-		writeNumber<short>(sizeof(SampleType) * 8);
+		writeNumber(4, 16);
+		writeNumber(2, 1);
+		writeNumber(2, 2);
+		writeNumber(4, sampleRate);
+		writeNumber(4, sampleRate * blockAlign);
+		writeNumber(2, blockAlign);
+		writeNumber(2, sizeof(SampleType) * 8);
 		outFile << "data";
-		writeNumber<int>(dataSize);
+		writeNumber(4, dataSize);
 		//samples.clear();
 		//samples = vector<short>(1000);
 		outFile.write(reinterpret_cast<const char*>(&samples[0]), dataSize);
